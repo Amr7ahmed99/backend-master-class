e8cef44d062fa23ea6edaec633426e89f57b8bc6
Amr7ahmed99 <amr.omar@maxab.io>
2023-12-24 15:46:47 +0200

    feat: Load config from file environment variables.

--- main.go
+++ main.go
@@ -4,19 +4,29 @@ import (
 	"backend-master-class/api"
 	"backend-master-class/db/connection"
 	db "backend-master-class/db/sqlc"
+	"backend-master-class/util"
 	"database/sql"
 	"log"
+
+	"github.com/spf13/viper"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	var connectionDB *sql.DB
-	const serverAddress = "0.0.0.0:8080"
-	connectionDB = connection.Postgres()
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatal("Config file not found:", err)
+		} else {
+			log.Fatal("Config file was found but another error was produced:", err)
+		}
+	}
+	connectionDB = connection.Postgres(config.DBDriver, config.DBSource)
 	store := db.NewStore(connectionDB)
 	server := api.NewServer(store)
-	if err := server.Start(serverAddress); err != nil {
+	if err := server.Start(config.ServerAddress); err != nil {
 		log.Fatal("can not start server:", err)
 	}
 }
